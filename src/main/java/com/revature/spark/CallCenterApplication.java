4e88cc429c4db110481e4adcd3c18fd8c8159f58
Patrick Walsh <patrick.walsh@revature.com>
2019-01-17 10:19:33 -0500

    swagger

--- src/main/java/com/revature/spark/CallCenterApplication.java
+++ src/main/java/com/revature/spark/CallCenterApplication.java
@@ -1,4 +1,4 @@
-package com.revature.spark.callcenter;
+package com.revature.spark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
