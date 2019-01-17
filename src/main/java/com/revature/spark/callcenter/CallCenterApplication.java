0b3fc8d88fa42d707642f09e36a3fdd14f0e6728
Patrick Walsh <patrick.walsh@revature.com>
2019-01-17 10:15:03 -0500

    initial commit

---
+++ src/main/java/com/revature/spark/callcenter/CallCenterApplication.java
@@ -0,0 +1,14 @@
+package com.revature.spark.callcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCenterApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/revature/spark/callcenter/CallCenterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.revature.spark.callcenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CallCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
