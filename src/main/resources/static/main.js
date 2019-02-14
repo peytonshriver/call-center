c2a467ee713fb2053f290bd2da45122610517837
Peyton Shriver <31674473+peytonshriver@users.noreply.github.com>
2019-02-13 19:15:07 -0500

    Got dropdown box working, may tweak button appearance functionality, but focusing on styling and presentation

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,4 +1,27 @@
-function ajaxReq(command,extension,callback,body){
+//establishing event listeners that are page specific
+//for info.html
+if(window.location.pathname == "/info.html"){
+    document.getElementById("selectTable").addEventListener("change", selectTable);
+    document.addEventListener('DOMContentLoaded', getAllCalls)
+}
+//event listener helper function
+function selectTable(){// helper function for the event listener on the drop-down box in info.html;
+    let selectTable = document.getElementById("selectTable");
+    document.getElementsByClassName("table")[0].innerHTML = "";
+    if(selectTable.value == "All Calls")
+        getAllCalls();
+    if(selectTable.value == "Call Stats")
+        makeCallStatTable();
+    if(selectTable.value == "Average Call Time Per User")
+        getAvgCallTimePerUser();
+}
+//for crud.html
+let userDropDown;
+if(window.location.pathname == "/crud.html"){
+     createUserDropdown(); // declaring a global variable because Ill need the same value cross function... this was to avoid an unrealiable timeout function like in makeCallStatTable
+    document.addEventListener("DOMContentLoaded", getDynamicCallTable);
+}
+function ajaxReq(command,extension,callback,body){//Ajax request that handles all instances of AJAX
     let url ="http://localhost:9595" + extension;
     let xhr = new XMLHttpRequest();
     xhr.open(command,url);
@@ -11,7 +34,7 @@ function ajaxReq(command,extension,callback,body){
             callback(xhr);
             return;
         }
-        if(this.readyState==4){
+        if(this.readyState==4){//This line could be better, but it gets the job done in the current context
             callback(xhr)
         }
     }
@@ -63,8 +86,8 @@ function makeCallStatTable(){
     row.insertCell(3).innerHTML= "Minimum Call Time";
     row.insertCell(4).innerHTML = "Sum of All Call Times";
     row = table[0].insertRow();
-    setTimeout(waitASec, 100);
-    function waitASec(){
+    setTimeout(waitASec, 100);// delays finishing the table until the 
+    function waitASec(){//       above ajax request are done processing
     row.insertCell(0).innerHTML = avg.toFixed(2);
     row.insertCell(1).innerHTML = max;
     row.insertCell(2).innerHTML = median;
@@ -73,6 +96,7 @@ function makeCallStatTable(){
     }
 }
 var min, max, avg, median, sum; //Declaring global variables
+//Processing functions that set their respective global variables to the values of them from the database
 function getMax(xhr){
     max = JSON.parse(xhr.response)
 }
@@ -117,7 +141,6 @@ function makeAvgCallTimePerUserTable(dataObj){
 function getDynamicCallTable(){
     ajaxReq("GET","/call/all",makeDynamicCallTable);
 }
-
 function makeDynamicCallTable(xhr){
     let allCalls = JSON.parse(xhr.response);
     let table = document.getElementsByClassName("table");
@@ -130,34 +153,40 @@ function makeDynamicCallTable(xhr){
     row.insertCell(5).innerHTML = "Options"
     let nCID; //announcing new call id
     for(let i=0; i<allCalls.length; i++){
-        if(i==allCalls.length-1)
+        if(i==allCalls.length-1)//makes it so the call id assigned to the new entry will not preceed the last entry of the table
             nCID = allCalls[i].id+1;
         let row = table[0].insertRow();
-        row.insertCell(0).innerHTML = "<p id='callID"+i+"'>"+allCalls[i].id+"</p>";
+        row.insertCell(0).innerHTML = "<p id='callID"+i+"'>"+allCalls[i].id+"</p>";//stand alone i is to make sure all the ids of collums are unique and correspond to the row number
         row.insertCell(1).innerHTML = "<input type='number' id='callTime"+i+"' value='"+allCalls[i].callTime+"'>";
         row.insertCell(2).innerHTML = "<textarea type='text' id='notes"+i+"' rows='4' cols='50'>"+allCalls[i].notes+"</textarea>";
         if(allCalls[i].resolved==true)
             row.insertCell(3).innerHTML = "<select id='resolved"+i+"'><option value='true'>true</option><option value='false'>false</option></select>";
         else
             row.insertCell(3).innerHTML = "<select id='resolved"+i+"'><option value='true'>true</option><option selected value='false'>false</option></select>";
-        //document.getElementById("resolved"+i).selected = allCalls[i].resolved;
-        row.insertCell(4).innerHTML = "User ID:   " + "<input type='number' id='userID"+i+"' value='"+allCalls[i].user.id+"'>" + "<br>" + "First Name: " + "<input type='text' id='firstName"+i+"' value='"+allCalls[i].user.firstName+"'>"+ " <br> Last Name:" + "<input type='text' id='lastName"+i+"' value='"+allCalls[i].user.lastName+"'>";
+        row.insertCell(4).innerHTML = "<select id='userID"+i+"'>"+userDropDown.toString()+"</select>";
         row.insertCell(5).innerHTML = "<button id='update"+i+"' onClick='doUpdate(this.id)'>Update</button><br><button id='delete"+i+"'onClick='doDelete(this.id)'>Delete</button>";
     }
-    let i = allCalls.length;
-    row = table[0].insertRow();
+    for(let i=0; i<allCalls.length; i++){//This loops through every select tag and sets its default value to its appropriate value
+        let options = document.getElementById("userID"+i);
+        for(let j = 0; j<options.length; j++){
+            if(allCalls[i].user.id == options[j].value)
+                options[j].setAttribute("selected","true"); 
+        }
+    }
+    let i = allCalls.length;// The rest of this code creates the last row of the table for crud.html
+    row = table[0].insertRow();// This row is responsible for taking new info to create a new call
     row.insertCell(0).innerHTML = "<p id='callID"+i+"'>"+nCID+"</p>";
     row.insertCell(1).innerHTML = "<input type='number' id='callTime"+i+"'>";
     row.insertCell(2).innerHTML = "<textarea type='text' id='notes"+i+"' rows='4' cols='50'></textarea>";
     row.insertCell(3).innerHTML = "<select id='resolved"+i+"'><option value='true'>true</option><option value='false'>false</option></select>";
-    row.insertCell(4).innerHTML = "User ID:   " + "<input type='number' id='userID"+i+"'>" + "<br>" + "First Name: " + "<input type='text' id='firstName"+i+"'>"+ " <br> Last Name:" + "<input type='text' id='lastName"+i+"'>";
+    row.insertCell(4).innerHTML = "<select id='userID"+i+"'>"+userDropDown+"</select>"
     row.insertCell(5).innerHTML = "<button id='create"+i+"' onClick='doCreate(this.id)'>Create</button>";
 }
+//functions for getting bodies of ajax request and sending them
 function doDelete(bId){
-    let i = String(bId).split("e")[3];
+    let i = String(bId).split("e")[3]; //gets the row number of the id from the button id
     let body = getBody(i);
     ajaxReq("DELETE","/call",alertGood, body);
-
 }
 function doUpdate(bId){
     let i = String(bId).split("e")[1];
@@ -169,61 +198,31 @@ function doCreate(bId){
     let body = getBody(i);
     ajaxReq("POST","/call",alertGood, body);
 }
-function alertGood(xhr){
+function alertGood(xhr){//Tells the user if the update,delete, or create was successful or not
     if(xhr.status==201 || xhr.status== 204){
         alert("Success");
     }else{
         alert("Action failed, try again!");}
-    document.getElementsByClassName("table")[0].innerHTML = "";
-    getDynamicCallTable();
-
-
+    document.getElementsByClassName("table")[0].innerHTML = "";//clears the table
+    getDynamicCallTable();//gets most recent version of the table
 }
-function  getBody(i){
+function  getBody(i){ //gets the body of the POST,PUT,and DELETE request based on the location of the button that is pushed
     let body= { callTime: document.getElementById("callTime"+i).value,
         id: document.getElementById("callID"+i).innerHTML,
-        notes: document.getElementById("notes"+i).value,
+        notes: document.getElementById("notes"+i).value,// notice it gets the values from the correct boxes based on row number
         resolved: document.getElementById("resolved"+i).value,
         user: {
-          firstName: document.getElementById("firstName"+i).value,
-          id: document.getElementById("userID"+i).value,
-          lastName: document.getElementById("lastName"+i).value
-        }
+          id: document.getElementById("userID"+i).value,  //only gets id because that is all that is necessary in the body of
+        }//                                                 PUT,POST, and DELETE methods being used
     }
-    return body;
-    
+    return body;   
 }
-let userDropDown;
-function createUserDropdown(){
+function createUserDropdown(){//helper function to make the AJAX call to get all users
     ajaxReq("GET","/user/all",makeUserDropDown);
 }
-function makeUserDropDown(xhr){
+function makeUserDropDown(xhr){//makes the options for the user drop-down box
     let allUsers = JSON.parse(xhr.response);
-
     for(let i = 0; i<allUsers.length; i++){
-        userDropDown += "<option uID='"+allUsers[0].id+"'>"+allUsers[0].id+". "+allUsers[i].firstName+" "+allUsers[i].lastName+"<option>";
+        userDropDown += "<option value='"+allUsers[i].id+"'>"+allUsers[i].id+". "+allUsers[i].firstName+" "+allUsers[i].lastName+"</option>";
     }
-
-}
-
-//event listeners
-//for info.html
-if(window.location.pathname == "/info.html"){
-    document.getElementById("selectTable").addEventListener("change", selectTable);
-    document.addEventListener('DOMContentLoaded', getAllCalls)
-}
-//for crud.html
-if(window.location.pathname == "/crud.html")
-    document.addEventListener("DOMContentLoaded", getDynamicCallTable)
-//event listener helper function
-function selectTable(){
-    let selectTable = document.getElementById("selectTable");
-    document.getElementsByClassName("table")[0].innerHTML = "";
-    if(selectTable.value == "All Calls")
-        getAllCalls();
-    if(selectTable.value == "Call Stats")
-        makeCallStatTable();
-    if(selectTable.value == "Average Call Time Per User")
-        getAvgCallTimePerUser();
-    // add other values and functions associated with them
 }
